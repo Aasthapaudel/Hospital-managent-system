3b477e503aa53f2855e14fac002fec158542c95e
Aastha <pdlpangali111@gmail.com>
2024-05-31 20:34:36 +0545

    auth added

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,16 @@ use App\Http\Controllers\AdminController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/home',[homeController::class,'redirect']);
+Route::get('/home',[homeController::class,'redirect'])->middleware('auth');
 Route::get('/',[homeController::class,'index']);
-Route::get('/add_doctor',[AdminController::class,'addview']);
-Route::get('/adminhome',[AdminController::class,'index']);
-Route::post('/upload_doctor',[AdminController::class,'upload']);
-Route::post('/appointment',[HomeController::class,'appointment']);
-Route::get('/getappointment',[HomeController::class,'getappointment']);
-Route::get('/doctor',[HomeController::class,'doctor']);
-Route::get('/latest',[HomeController::class,'latest']);
-Route::get('/contact',[HomeController::class,'contact']);
+Route::get('/add_doctor',[AdminController::class,'addview'])->middleware('auth');
+Route::get('/adminhome',[AdminController::class,'index'])->middleware('auth');
+Route::post('/upload_doctor',[AdminController::class,'upload'])->middleware('auth');
+Route::post('/appointment',[HomeController::class,'appointment'])->middleware('auth');
+Route::get('/getappointment',[HomeController::class,'getappointment'])->middleware('auth');
+Route::get('/doctor',[HomeController::class,'doctor'])->middleware('auth');
+Route::get('/latest',[HomeController::class,'latest'])->middleware('auth');
+Route::get('/contact',[HomeController::class,'contact'])->middleware('auth');
 Route::get('/about',[HomeController::class,'about']);
 
 
